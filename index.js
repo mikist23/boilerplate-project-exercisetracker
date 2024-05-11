87084d054ac1b3d118ca877651ccb48a5866de8c
Absai Mike Ochango <mikeochango3@gmail.com>
2024-05-11 23:29:33 +0000

    Provide dtabase conection

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+  console.log("Conected to database")
+}).catch((err)=>{
+  console.log(err)
+});
+const { Schema } = mongoose;
 
 
 
